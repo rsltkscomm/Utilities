c98ebffe4b7cb7e4d06397b3e15c35e4fdbcf09d
resqateam02 <70644035+resqateam02@users.noreply.github.com>
2025-05-13 11:06:23 +0530

    Update NoProdMethodSkipper.java

--- src/main/java/org/utility/NoProdMethodSkipper.java
+++ src/main/java/org/utility/NoProdMethodSkipper.java
@@ -18,6 +18,8 @@ public class NoProdMethodSkipper implements IInvokedMethodListener
 		{
 			if (!System.getProperty("restrictRun").equalsIgnoreCase("yes"))
 			{
+				throw new SkipException("This method is disabled in the production environment.");
+			}
 			if (System.getProperty("ProdRun").equalsIgnoreCase("no") && System.getProperty("Environment").equalsIgnoreCase("run"))
 			{
 				throw new SkipException("This method is disabled in the production environment.");
