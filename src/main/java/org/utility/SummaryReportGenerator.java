a5588eaff996a69ec8dec58d4781bf9e107692f9
resqateam05 <res_qateam05@resulticksmail.com>
2025-05-21 16:52:41 +0530

    Custom report update

--- src/main/java/org/utility/SummaryReportGenerator.java
+++ src/main/java/org/utility/SummaryReportGenerator.java
@@ -193,12 +193,6 @@ public class SummaryReportGenerator
 
 	public static String getStyle(String productName)
 	{
-		// if (productName.toLowerCase().contains("marketing star"))
-		// {
-		// 	return "style=\"margin: 1px;padding-left: 5px;padding-bottom: 70px;height: 140px;width: 400px;\"";
-		// }else {
-		// 	return "style=\"style=\"margin: 5px;padding-left: 10px;padding-bottom: 30px;height: 80px;width: 300px;\"";
-		// }
 		return "";
 	}
 	
